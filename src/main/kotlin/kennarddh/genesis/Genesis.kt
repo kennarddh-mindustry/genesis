45a39eb2b89a253683cc72df41bdd1876e42e5ce
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-07 00:13:54 +0700

    feat: Command registry

--- src/main/kotlin/kennarddh/genesis/Genesis.kt
+++ src/main/kotlin/kennarddh/genesis/Genesis.kt
@@ -1,25 +1,17 @@
 package kennarddh.genesis
 
 import arc.util.Log
-import arc.util.Reflect
-import kennarddh.genesis.commands.InterceptedCommandHandler
+import kennarddh.genesis.commands.CommandRegistry
 import mindustry.mod.Plugin
-import mindustry.server.ServerControl
 
 @SuppressWarnings("unused")
 class Genesis : Plugin() {
+
     override fun init() {
         Log.info("[Genesis] Loaded")
 
-//        println(ServerControl.instance.handler.handleMessage("mods"))
+        val commandRegistry = CommandRegistry()
 
-        Reflect.set(ServerControl.instance, "handler", InterceptedCommandHandler("") { command, player ->
-            println(command)
-            println(player)
-        })
-//        println(ServerControl.instance.handler.handleMessage("mods"))
-//        ServerControl.instance.handler = CustomCommandHandler("")
-
-//        Vars.netServer.clientCommands
+        commandRegistry.init()
     }
 }
--- src/main/kotlin/kennarddh/genesis/commands/CommandRegistry.kt
+++ src/main/kotlin/kennarddh/genesis/commands/CommandRegistry.kt
@@ -1,3 +1,20 @@
 package kennarddh.genesis.commands
 
-class CommandRegistry
+import arc.util.Reflect
+import mindustry.Vars
+
+class CommandRegistry {
+    fun init() {
+//        Reflect.set(ServerControl.instance, "handler", InterceptedCommandHandler("") { command, player ->
+//            println("server")
+//            println(command)
+//            println(player)
+//        })
+
+        Reflect.set(Vars.netServer, "clientCommands", InterceptedCommandHandler("/") { command, player ->
+            println("/client")
+            println(command)
+            println(player)
+        })
+    }
+}
