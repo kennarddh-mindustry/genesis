08dd373c016c496a363bfc3fe9dd74e3a8ac2816
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-06 12:12:33 +0700

    chore: Setup

--- src/main/kotlin/kennarddh/genisis/Genesis.kt
+++ src/main/kotlin/kennarddh/genisis/Genesis.kt
@@ -1,11 +1,11 @@
-package kennarddh
+package kennarddh.genisis
 
 import arc.util.Log
 import mindustry.mod.Plugin
 
 @SuppressWarnings("unused")
-class Main : Plugin() {
+class Genesis : Plugin() {
     override fun init() {
-        Log.info("[MindustryPluginBoilerplate] Loaded")
+        Log.info("[Genesis] Loaded")
     }
 }
