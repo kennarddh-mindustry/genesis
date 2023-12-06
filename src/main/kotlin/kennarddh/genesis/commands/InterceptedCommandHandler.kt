bc24992af34eeb240f4c83456994be5f9d64dcd2
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-06 23:33:05 +0700

    feat: Intercepted command handler with prefix

--- src/main/kotlin/kennarddh/genesis/commands/InterceptedCommandHandler.kt
+++ src/main/kotlin/kennarddh/genesis/commands/InterceptedCommandHandler.kt
@@ -11,7 +11,12 @@ val DummyValidCommandResponse = CommandHandler.CommandResponse(
 
 class InterceptedCommandHandler(prefix: String?, private val onIntercept: (String, Player?) -> Unit) :
     CommandHandler(prefix) {
-    override fun handleMessage(command: String, params: Any?): CommandResponse {
+    override fun handleMessage(commandWithPrefix: String, params: Any?): CommandResponse {
+        if (!commandWithPrefix.startsWith(prefix))
+            return DummyValidCommandResponse
+
+        val command = commandWithPrefix.substring(prefix.length)
+
         onIntercept(command, if (params is Player) params else null)
 
         return DummyValidCommandResponse
