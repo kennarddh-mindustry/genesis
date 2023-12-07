58ac216902091d5973dd663ca54c483ffbe57c55
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-07 20:17:57 +0700

    feat: Command data parameters as KClass list

--- src/main/kotlin/kennarddh/genesis/commands/CommandData.kt
+++ src/main/kotlin/kennarddh/genesis/commands/CommandData.kt
@@ -1,14 +1,14 @@
 package kennarddh.genesis.commands
 
-import kennarddh.genesis.commands.parameters.base.CommandParameterConverter
 import kennarddh.genesis.handlers.Handler
 import java.lang.reflect.Method
+import kotlin.reflect.KClass
 
 data class CommandData(
     val sides: Array<CommandSide>,
     val handler: Handler,
     val method: Method,
-    val parameters: Array<CommandParameterConverter<*>>
+    val parameters: List<KClass<*>>
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -19,7 +19,7 @@ data class CommandData(
         if (!sides.contentEquals(other.sides)) return false
         if (handler != other.handler) return false
         if (method != other.method) return false
-        if (!parameters.contentEquals(other.parameters)) return false
+        if (parameters != other.parameters) return false
 
         return true
     }
@@ -28,8 +28,7 @@ data class CommandData(
         var result = sides.contentHashCode()
         result = 31 * result + handler.hashCode()
         result = 31 * result + method.hashCode()
-        result = 31 * result + parameters.contentHashCode()
+        result = 31 * result + parameters.hashCode()
         return result
     }
-
 }
