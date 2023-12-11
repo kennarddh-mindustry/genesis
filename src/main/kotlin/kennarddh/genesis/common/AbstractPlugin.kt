977fd4aa5decab6c84f156100f8e973c2cde41ec
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-11 23:23:43 +0700

    feat: Hide abstract plugin method with deprecated annotation

--- src/main/kotlin/kennarddh/genesis/common/AbstractPlugin.kt
+++ src/main/kotlin/kennarddh/genesis/common/AbstractPlugin.kt
@@ -4,9 +4,12 @@ import arc.util.CommandHandler
 import mindustry.mod.Plugin
 
 abstract class AbstractPlugin : Plugin() {
+    @Deprecated("This won't add command.", ReplaceWith("CommandRegistry"), DeprecationLevel.HIDDEN)
     final override fun registerClientCommands(handler: CommandHandler?) = Unit
 
+    @Deprecated("This won't add command.", ReplaceWith("CommandRegistry"), DeprecationLevel.HIDDEN)
     final override fun registerServerCommands(handler: CommandHandler?) = Unit
 
+    @Deprecated("DO NOT USE OR CALL THIS", ReplaceWith("Plugin only. This is for mod."), DeprecationLevel.HIDDEN)
     final override fun loadContent() = Unit
 }
