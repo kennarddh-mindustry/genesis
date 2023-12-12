6b54a814f2703b22adf31675605860d406c6615c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-12 20:38:44 +0700

    chore(common): Use toxopid

--- genesis-common/build.gradle.kts
+++ genesis-common/build.gradle.kts
@@ -1,17 +1,19 @@
-import groovy.json.JsonBuilder
-import groovy.json.JsonSlurper
+import fr.xpdustry.toxopid.dsl.mindustryDependencies
+import fr.xpdustry.toxopid.spec.ModMetadata
+import fr.xpdustry.toxopid.spec.ModPlatform
+import fr.xpdustry.toxopid.task.GithubArtifactDownload
 
 plugins {
     kotlin("jvm") version "1.9.10"
     `maven-publish`
+    id("fr.xpdustry.toxopid") version "3.2.0"
 }
 
 repositories {
     mavenLocal()
     mavenCentral()
-    maven("https://raw.githubusercontent.com/Zelaux/MindustryRepo/master/repository")
-    maven("https://www.jitpack.io")
     maven("https://maven.xpdustry.com/releases")
+    maven("https://maven.xpdustry.com/mindustry")
 
     maven {
         url = uri("http://23.95.107.12:9999/releases")
@@ -19,21 +21,28 @@ repositories {
     }
 }
 
-val mindustryVersion = "v146"
-val jabelVersion = "93fde537c7"
-val submoduleName = "genesis-common"
+toxopid {
+    // The version with which your mod/plugin is compiled.
+    // If not set, will compile with v143 by default.
+    compileVersion.set("v146")
+    // The version with which your mod/plugin is tested.
+    // If not set, defaults to the value of compileVersion.
+    runtimeVersion.set("v146")
 
-group = "kennarddh"
-version = rootProject.file("$submoduleName/version.txt").readLines().first()
+    // The platforms you target, you can choose DESKTOP, HEADLESS or/and ANDROID.
+    // If not set, will target DESKTOP by default.
+    platforms.add(ModPlatform.HEADLESS)
+}
+
+val metadata = ModMetadata.fromJson(project.file("plugin.json"))
+
+project.group = "kennarddh"
+project.version = metadata.version
 
 dependencies {
-    compileOnly("com.github.Anuken.Arc:arc-core:$mindustryVersion")
-    compileOnly("com.github.Anuken.Mindustry:core:$mindustryVersion")
-    compileOnly("com.github.Anuken.Mindustry:server:$mindustryVersion")
-    annotationProcessor("com.github.Anuken:jabel:$jabelVersion")
+    mindustryDependencies()
 
     compileOnly("com.xpdustry:kotlin-runtime:3.1.0-k.1.9.10")
-    compileOnly(kotlin("reflect"))
 
     compileOnly("kennarddh:genesis-core:1.0.1")
 }
@@ -55,27 +64,33 @@ configurations.runtimeClasspath {
 
 tasks {
     jar {
-        val pluginJson = rootProject.file("$submoduleName/src/main/resources/plugin.json")
-        val pluginJsonText = pluginJson.readText()
-        val jsonSlurper = JsonSlurper()
+        doFirst {
+            val metadataFile = temporaryDir.resolve("plugin.json")
 
-        @Suppress("UNCHECKED_CAST")
-        val pluginJsonParsed = jsonSlurper.parseText(pluginJsonText) as MutableMap<Any, Any>
+            metadataFile.writeText(metadata.toJson(true))
 
-        pluginJsonParsed["version"] = version
-
-        val builder = JsonBuilder(pluginJsonParsed)
-
-        val pluginJsonModified = builder.toPrettyString()
-
-        pluginJson.writeText(pluginJsonModified)
+            from(metadataFile)
+        }
 
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
         from(sourceSets.main.get().output)
         from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-        from(rootProject.fileTree("src/main/resources/"))
     }
+}
+
+val downloadKotlinRuntime =
+    tasks.register<GithubArtifactDownload>("downloadKotlinRuntime") {
+        user.set("xpdustry")
+        repo.set("kotlin-runtime")
+        name.set("kotlin-runtime.jar")
+        version.set("v3.1.0-k.1.9.10")
+    }
+
+val genesisCorePlugin = project.file("../genesis-core/build/libs/genesis-core-1.0.1.jar")
+
+tasks.runMindustryServer {
+    mods.setFrom(setOf(tasks.jar, downloadKotlinRuntime, genesisCorePlugin))
 }
 
 publishing {
@@ -93,7 +108,7 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = groupId
-            artifactId = submoduleName
+            artifactId = metadata.name
             version = version
             from(components["java"])
         }
