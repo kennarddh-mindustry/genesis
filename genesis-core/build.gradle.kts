a00bd98ec29e3641e10d0ac7e345742eb802438a
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-12 13:10:07 +0700

    chore: Publish gradle

--- genesis-core/build.gradle.kts
+++ genesis-core/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.10"
+    `maven-publish`
 }
 
 group = "kennarddh"
-version = "1.0"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -20,7 +21,7 @@ dependencies {
     compileOnly("com.github.Anuken.Mindustry:core:$mindustryVersion")
     compileOnly("com.github.Anuken.Mindustry:server:$mindustryVersion")
     annotationProcessor("com.github.Anuken:jabel:$jabelVersion")
-    
+
     compileOnly("com.xpdustry:kotlin-runtime:3.1.0-k.1.9.10")
     compileOnly(kotlin("reflect"))
 }
@@ -46,4 +47,26 @@ tasks.register<Jar>("buildJAR") {
     from(sourceSets.main.get().output)
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
     from(rootProject.fileTree("src/main/resources/"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "reposilite"
+            url = uri("http://23.95.107.12:9999/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            isAllowInsecureProtocol = true
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "kennarddh"
+            artifactId = "genesis-core"
+            version = version
+            from(components["java"])
+        }
+    }
 }
