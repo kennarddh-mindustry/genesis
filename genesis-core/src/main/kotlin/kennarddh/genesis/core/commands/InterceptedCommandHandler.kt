20098ef1813e985a39e88cf3015cae5894fdaef5
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-13 21:00:17 +0700

    feat(core): Hide command handler methods

--- genesis-core/src/main/kotlin/kennarddh/genesis/core/commands/InterceptedCommandHandler.kt
+++ genesis-core/src/main/kotlin/kennarddh/genesis/core/commands/InterceptedCommandHandler.kt
@@ -29,4 +29,19 @@ class InterceptedCommandHandler(prefix: String?, private val onIntercept: (Strin
 
         return dummyValidCommandResponse
     }
+
+    @Deprecated("Do not use this with genesis", ReplaceWith("Genesis command annotation"), DeprecationLevel.HIDDEN)
+    override fun removeCommand(text: String?) {
+        throw UnsupportedOperationException("Do not use this with genesis. Replace it with Genesis command annotation. If you are not the mod creator, This mod can't be used with Genesis because Genesis override mindustry command system.")
+    }
+
+    @Deprecated("Do not use this with genesis", ReplaceWith("Genesis command annotation"), DeprecationLevel.HIDDEN)
+    override fun <T : Any?> register(
+        text: String?,
+        params: String?,
+        description: String?,
+        runner: CommandRunner<T>?
+    ): Command {
+        throw UnsupportedOperationException("Do not use this with genesis. Replace it with Genesis command annotation. If you are not the mod creator, This mod can't be used with Genesis because Genesis override mindustry command system.")
+    }
 }
