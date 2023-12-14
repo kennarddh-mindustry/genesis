741d78370e85d159f4a77f7f3f66d3e0f2cac2fb
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-15 00:17:12 +0700

    refactor(core): Rename MutablePriorityList

--- genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/MutablePriorityList.kt
+++ genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/MutablePriorityList.kt
@@ -1,6 +1,6 @@
 package kennarddh.genesis.core.commons
 
-class PriorityMutableList<E> : ArrayList<PriorityContainer<E>>(), MutableList<PriorityContainer<E>> {
+class MutablePriorityList<E> : ArrayList<PriorityContainer<E>>(), MutableList<PriorityContainer<E>> {
     fun forEachPrioritized(action: (E) -> Unit) {
         this.sorted().forEach { action(it.data) }
     }
