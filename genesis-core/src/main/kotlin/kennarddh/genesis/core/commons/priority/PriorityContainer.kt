cee5e23267bef59ea858ad2bfe0afb47c47c0e9c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-15 16:30:39 +0700

    fix(core): Priority compare

--- genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/priority/PriorityContainer.kt
+++ genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/priority/PriorityContainer.kt
@@ -2,7 +2,7 @@ package kennarddh.genesis.core.commons.priority
 
 data class PriorityContainer<T>(val priority: PriorityEnum, val data: T) : Comparable<PriorityContainer<T>> {
     override fun compareTo(other: PriorityContainer<T>): Int {
-        return priority.value - other.priority.value
+        return other.priority.value - priority.value
     }
 
 }
--- genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/priority/PriorityEnum.kt
+++ genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/priority/PriorityEnum.kt
@@ -1,9 +1,9 @@
 package kennarddh.genesis.core.commons.priority
 
 enum class PriorityEnum(val value: Int) {
-    VeryLow(10),
-    Low(20),
-    Normal(30),
-    High(40),
     Important(50),
+    High(40),
+    Normal(30),
+    Low(20),
+    VeryLow(10),
 }
