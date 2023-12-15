e95e7112f8714c1c18ffac1c1b54b80956d52f9b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-16 02:06:49 +0700

    feat(common): Enum parameter

---
+++ genesis-common/src/main/kotlin/kennarddh/genesis/common/commands/parameters/types/EnumParameter.kt
@@ -0,0 +1,19 @@
+package kennarddh.genesis.common.commands.parameters.types
+
+import kennarddh.genesis.core.commands.parameters.types.CommandParameter
+import kennarddh.genesis.core.commands.parameters.types.CommandParameterParsingException
+import kotlin.reflect.KClass
+
+class EnumParameter<T : Enum<*>> : CommandParameter<T> {
+    override fun parse(instance: KClass<T>, input: String): T {
+        // TODO: https://youtrack.jetbrains.com/issue/KT-14743
+        return instance.java.enumConstants.find { it.name == input }
+            ?: throw CommandParameterParsingException("$input is not a valid value for :parameterName: parameter enum.")
+    }
+
+    override fun toUsageType(input: KClass<T>): String {
+        val validValues = input.java.enumConstants.map { it.name }
+
+        return validValues.joinToString("|")
+    }
+}
