577e5112b01ba2e0b4377982a6f764e0eb978f2a
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-28 00:44:06 +0700

    refactor(core): Change load content hiding message

--- genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/AbstractPlugin.kt
+++ genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/AbstractPlugin.kt
@@ -3,6 +3,6 @@ package kennarddh.genesis.core.commons
 import mindustry.mod.Plugin
 
 abstract class AbstractPlugin : Plugin() {
-    @Deprecated("Useless", ReplaceWith("Plugin only. This is for mod."), DeprecationLevel.HIDDEN)
+    @Deprecated("Plugin only. This is for mod.", ReplaceWith("Plugin only. This is for mod."), DeprecationLevel.HIDDEN)
     final override fun loadContent() = Unit
 }
