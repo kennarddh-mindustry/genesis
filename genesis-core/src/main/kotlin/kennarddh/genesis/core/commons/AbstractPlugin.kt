b24f8b4a7fb810cdce148b3d54c69b8b36678f73
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-28 00:41:15 +0700

    feat(core): Remove mindustry command register hiding

--- genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/AbstractPlugin.kt
+++ genesis-core/src/main/kotlin/kennarddh/genesis/core/commons/AbstractPlugin.kt
@@ -1,15 +1,8 @@
 package kennarddh.genesis.core.commons
 
-import arc.util.CommandHandler
 import mindustry.mod.Plugin
 
 abstract class AbstractPlugin : Plugin() {
-    @Deprecated("This won't add command.", ReplaceWith("CommandRegistry"), DeprecationLevel.HIDDEN)
-    final override fun registerClientCommands(handler: CommandHandler?) = Unit
-
-    @Deprecated("This won't add command.", ReplaceWith("CommandRegistry"), DeprecationLevel.HIDDEN)
-    final override fun registerServerCommands(handler: CommandHandler?) = Unit
-
-    @Deprecated("DO NOT USE OR CALL THIS", ReplaceWith("Plugin only. This is for mod."), DeprecationLevel.HIDDEN)
+    @Deprecated("Useless", ReplaceWith("Plugin only. This is for mod."), DeprecationLevel.HIDDEN)
     final override fun loadContent() = Unit
 }
