6af74e4e5fb583ac1e55e85aa0db81e51a906cd9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-12-31 11:32:12 +0700

    fix(core): Change timer task annotation default parameter

--- genesis-core/src/main/kotlin/kennarddh/genesis/core/timers/annotations/TimerTask.kt
+++ genesis-core/src/main/kotlin/kennarddh/genesis/core/timers/annotations/TimerTask.kt
@@ -6,4 +6,4 @@ package kennarddh.genesis.core.timers.annotations
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
-annotation class TimerTask(val delaySeconds: Float, val intervalSeconds: Float = 0f, val repeatCount: Int = -1)
+annotation class TimerTask(val delaySeconds: Float, val intervalSeconds: Float = 0f, val repeatCount: Int = 0)
