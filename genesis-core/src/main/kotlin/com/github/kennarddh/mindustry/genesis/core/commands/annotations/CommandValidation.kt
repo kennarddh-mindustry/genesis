642906f28e2a263e3210a427e73746b69a882e42
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-27 01:30:03 +0700

    feat(core): Command validation annotation

---
+++ genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commands/annotations/CommandValidation.kt
@@ -0,0 +1,6 @@
+package com.github.kennarddh.mindustry.genesis.core.commands.annotations
+
+@Target(AnnotationTarget.ANNOTATION_CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+annotation class CommandValidation
--- genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commands/parameters/validations/ParameterValidation.kt
+++ genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commands/parameters/validations/ParameterValidation.kt
@@ -1,6 +1,5 @@
 package com.github.kennarddh.mindustry.genesis.core.commands.parameters.validations
 
-
 @Target(AnnotationTarget.ANNOTATION_CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
