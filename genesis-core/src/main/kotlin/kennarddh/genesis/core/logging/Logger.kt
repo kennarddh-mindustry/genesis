6dd258a61b537182cdafe611caa434299aa858b7
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-26 16:55:39 +0700

    fix(core): Wrong logger name

--- genesis-core/src/main/kotlin/kennarddh/genesis/core/logging/Logger.kt
+++ genesis-core/src/main/kotlin/kennarddh/genesis/core/logging/Logger.kt
@@ -3,4 +3,4 @@ package kennarddh.genesis.core.logging
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-internal val Logger: Logger = LoggerFactory.getLogger("ToastCore")
+internal val Logger: Logger = LoggerFactory.getLogger("GenesisCore")
