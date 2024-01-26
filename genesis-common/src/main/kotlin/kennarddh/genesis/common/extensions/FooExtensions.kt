7e7e883be9a38a51c7778bf4beafa78ebc0d1811
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-26 16:55:20 +0700

    feat(common): Player and foo extensions

---
+++ genesis-common/src/main/kotlin/kennarddh/genesis/common/extensions/FooExtensions.kt
@@ -0,0 +1,7 @@
+package kennarddh.genesis.common.extensions
+
+fun String.stripFooMessageInvisibleCharacters(): String =
+    if (this.takeLast(2).all { (0xF80..<0x107F).contains(it.code) })
+        this.dropLast(2)
+    else
+        this
