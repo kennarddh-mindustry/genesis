624616212d77afd68e53e400e8656a29f4b7da8b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-07 02:56:17 +0700

    feat: Inline for each prioritized

--- genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commons/priority/MutablePriorityList.kt
+++ genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commons/priority/MutablePriorityList.kt
@@ -1,7 +1,7 @@
 package com.github.kennarddh.mindustry.genesis.core.commons.priority
 
 class MutablePriorityList<E> : ArrayList<PriorityContainer<E>>(), MutableList<PriorityContainer<E>> {
-    fun forEachPrioritized(action: (E) -> Unit) {
-        this.sorted().forEach { action(it.data) }
+    inline fun forEachPrioritized(action: (E) -> Unit) {
+        sorted().forEach { action(it.data) }
     }
 }
