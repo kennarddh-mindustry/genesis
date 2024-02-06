1d4fea6afe12b24c90f1990a5bdf7742910a339e
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-07 03:16:50 +0700

    feat(standard): Run blocking plugin init

--- genesis-standard/src/main/kotlin/com/github/kennarddh/mindustry/genesis/standard/GenesisStandard.kt
+++ genesis-standard/src/main/kotlin/com/github/kennarddh/mindustry/genesis/standard/GenesisStandard.kt
@@ -5,13 +5,15 @@ import com.github.kennarddh.mindustry.genesis.core.commons.AbstractPlugin
 import com.github.kennarddh.mindustry.genesis.standard.handlers.commands.CommandsHandler
 import com.github.kennarddh.mindustry.genesis.standard.handlers.foo.FooHandler
 import com.github.kennarddh.mindustry.genesis.standard.handlers.server.ServerHandler
+import kotlinx.coroutines.runBlocking
 
 class GenesisStandard : AbstractPlugin() {
-    override fun init() {
-        Logger.info("Loaded")
+    override fun init() =
+        runBlocking {
+            Genesis.registerHandler(CommandsHandler())
+            Genesis.registerHandler(ServerHandler())
+            Genesis.registerHandler(FooHandler())
 
-        Genesis.registerHandler(CommandsHandler())
-        Genesis.registerHandler(ServerHandler())
-        Genesis.registerHandler(FooHandler())
-    }
+            Logger.info("Loaded")
+        }
 }
