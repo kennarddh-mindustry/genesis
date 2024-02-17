f93a43530fd48e1adb53b49a353701bb828d354f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-17 23:50:59 +0700

    fix(standard): Init

--- genesis-standard/src/main/kotlin/com/github/kennarddh/mindustry/genesis/standard/GenesisStandard.kt
+++ genesis-standard/src/main/kotlin/com/github/kennarddh/mindustry/genesis/standard/GenesisStandard.kt
@@ -5,10 +5,9 @@ import com.github.kennarddh.mindustry.genesis.core.commons.AbstractPlugin
 import com.github.kennarddh.mindustry.genesis.standard.handlers.commands.CommandsHandler
 import com.github.kennarddh.mindustry.genesis.standard.handlers.foo.FooHandler
 import com.github.kennarddh.mindustry.genesis.standard.handlers.server.ServerHandler
-import kotlinx.coroutines.runBlocking
 
 class GenesisStandard : AbstractPlugin() {
-    override fun init() = runBlocking {
+    override suspend fun onInit() {
         GenesisAPI.registerHandler(CommandsHandler())
         GenesisAPI.registerHandler(ServerHandler())
         GenesisAPI.registerHandler(FooHandler())
