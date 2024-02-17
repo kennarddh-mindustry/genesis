e0d3699a81ce22df8aea2efc1ba73c1e27e9379e
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-17 23:33:43 +0700

    feat!: Suspended init in abstract plugin

--- genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commons/AbstractPlugin.kt
+++ genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commons/AbstractPlugin.kt
@@ -1,10 +1,13 @@
 package com.github.kennarddh.mindustry.genesis.core.commons
 
+import kotlinx.coroutines.runBlocking
 import mindustry.mod.Plugin
 
 abstract class AbstractPlugin : Plugin() {
     @Deprecated("Plugin only. This is for mod.", ReplaceWith("Plugin only. This is for mod."), DeprecationLevel.HIDDEN)
     final override fun loadContent() = Unit
+
+    final override fun init() = runBlocking { onInit() }
 
     /**
      * Called when the Application is destroyed.
@@ -12,4 +15,9 @@ abstract class AbstractPlugin : Plugin() {
      * Will be run at the same time for every plugin's dispose including Handler onDispose.
      */
     open suspend fun dispose() = Unit
+
+    /**
+     * Will be fired at genesis plugin init and at the same time with every AbstractPlugin.
+     */
+    open suspend fun onInit() = Unit
 }
