b430da170d0e9fdb573bdadc6562b04eb7932e27
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-03-19 11:32:15 +0700

    refactor: Use object for StringParser

--- genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commons/StringParser.kt
+++ genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commons/StringParser.kt
@@ -9,73 +9,71 @@ open class StringParserToken
 data object SkipToken : StringParserToken()
 data class StringToken(val value: String) : StringParserToken()
 
-class StringParser {
-    companion object {
-        private val escapedCharactersMap = mapOf('n' to "\n", '\"' to "\"", '\\' to "\\", '*' to "*")
+object StringParser {
+    private val escapedCharactersMap = mapOf('n' to "\n", '\"' to "\"", '\\' to "\\", '*' to "*")
 
-        fun parse(input: String): Iterator<StringParserToken> =
-            iterator {
-                var isEscaping = false
-                var isInQuote = false
+    fun parse(input: String): Iterator<StringParserToken> =
+        iterator {
+            var isEscaping = false
+            var isInQuote = false
 
-                val output = buildString {
-                    for (char in input) {
-                        if (isEscaping) {
-                            if (escapedCharactersMap.contains(char)) {
-                                append(escapedCharactersMap[char])
+            val output = buildString {
+                for (char in input) {
+                    if (isEscaping) {
+                        if (escapedCharactersMap.contains(char)) {
+                            append(escapedCharactersMap[char])
 
-                                isEscaping = false
+                            isEscaping = false
 
-                                continue
-                            } else {
-                                throw InvalidEscapedCharacterException("$char is not a valid character to be escaped")
-                            }
-                        }
-
-                        when (char) {
-                            '\\' -> isEscaping = true
-                            '"' -> isInQuote = !isInQuote
-                            '*' -> {
-                                if (!isInQuote && isEmpty())
-                                    yield(SkipToken)
-                                else
-                                    append(char)
-                            }
-
-                            ' ' -> {
-                                if (!isInQuote) {
-                                    if (isNotEmpty()) {
-                                        yield(StringToken(toString()))
-                                        clear()
-                                    }
-                                } else
-                                    append(char)
-                            }
-
-                            else -> append(char)
+                            continue
+                        } else {
+                            throw InvalidEscapedCharacterException("$char is not a valid character to be escaped")
                         }
                     }
-                }
 
-                if (isEscaping) {
-                    throw InvalidEscapedCharacterException("No character provided after escape character")
-                } else if (isInQuote) {
-                    throw UnterminatedStringException("Double quoted string $output is not terminated")
-                } else if (output.isNotBlank()) {
-                    yield(StringToken(output.toString()))
+                    when (char) {
+                        '\\' -> isEscaping = true
+                        '"' -> isInQuote = !isInQuote
+                        '*' -> {
+                            if (!isInQuote && isEmpty())
+                                yield(SkipToken)
+                            else
+                                append(char)
+                        }
+
+                        ' ' -> {
+                            if (!isInQuote) {
+                                if (isNotEmpty()) {
+                                    yield(StringToken(toString()))
+                                    clear()
+                                }
+                            } else
+                                append(char)
+                        }
+
+                        else -> append(char)
+                    }
                 }
             }
 
-        fun parseToArray(input: String): Array<StringParserToken> {
-            val parsed = parse(input)
-
-            val output: MutableList<StringParserToken> = mutableListOf()
-
-            parsed.forEach {
-                output.add(it)
+            if (isEscaping) {
+                throw InvalidEscapedCharacterException("No character provided after escape character")
+            } else if (isInQuote) {
+                throw UnterminatedStringException("Double quoted string $output is not terminated")
+            } else if (output.isNotBlank()) {
+                yield(StringToken(output.toString()))
             }
-
-            return output.toTypedArray()
         }
+
+    fun parseToArray(input: String): Array<StringParserToken> {
+        val parsed = parse(input)
+
+        val output: MutableList<StringParserToken> = mutableListOf()
+
+        parsed.forEach {
+            output.add(it)
+        }
+
+        return output.toTypedArray()
     }
 }
