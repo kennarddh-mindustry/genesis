478768c747461a0a60015577fa9fe08fc0e1ea47
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-04-21 20:03:17 +0700

    feat(command-senders): Delete old ClientSide and ServerSide annotation

--- genesis-core/src/main/kotlin/com/github/kennarddh/mindustry/genesis/core/commands/annotations/ClientSide.kt
+++
@@ -1,6 +0,0 @@
-package com.github.kennarddh.mindustry.genesis.core.commands.annotations
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-@MustBeDocumented
-annotation class ClientSide
