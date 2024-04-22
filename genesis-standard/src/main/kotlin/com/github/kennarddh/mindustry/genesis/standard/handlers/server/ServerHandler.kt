bdff5da21d8c21c38079aecd78563ac9be205365
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-04-22 13:24:57 +0700

    fix: Player limit command name

--- genesis-standard/src/main/kotlin/com/github/kennarddh/mindustry/genesis/standard/handlers/server/ServerHandler.kt
+++ genesis-standard/src/main/kotlin/com/github/kennarddh/mindustry/genesis/standard/handlers/server/ServerHandler.kt
@@ -500,7 +500,7 @@ class ServerHandler : Handler {
         }
     }
 
-    @Command(["player-limit", "playerlimit"])
+    @Command(["player_limit", "playerlimit"])
     @Description("Set the server player limit. 0 to disable limit")
     suspend fun playerLimit(sender: ServerCommandSender, @GTE(0) limit: Int? = null) {
         if (limit == null)
