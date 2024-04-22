0f185eff0e5a8cf763af8e1e46332c0c2b1a6b20
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-04-22 11:07:22 +0700

    feat: Add string extensions

---
+++ genesis-standard/src/main/kotlin/com/github/kennarddh/mindustry/genesis/standard/extensions/StringExtensions.kt
@@ -0,0 +1,7 @@
+package com.github.kennarddh.mindustry.genesis.standard.extensions
+
+import arc.util.Strings
+
+fun String.stripColors(): String = Strings.stripColors(this)
+
+fun String.stripGlyphs(): String = Strings.stripGlyphs(this)
